a197229d0c29a4e52d584339693e721d9ddb48b3
Shishir Shekhar <sspdav02@gmail.com>
2024-08-19 21:16:34 +0530

    feat(page): add navbar and hero to home page

---
+++ client/app/(routes)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Home/Hero/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
