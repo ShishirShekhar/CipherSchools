bae4cb879f795e1a4e117d44439a73275e7adb24
Shishir Shekhar <sspdav02@gmail.com>
2024-08-20 23:36:50 +0530

    feat(util): add util to check auth

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export async function checkAuth() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+            mode: "cors",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+        })
+
+        if (res.ok) {
+            const user = await res.json();
+            return user;
+        }
+
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
