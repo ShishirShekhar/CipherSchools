05d8f3bed75816d11205fad8a7109e46ddd02a8f
Shishir Shekhar <sspdav02@gmail.com>
2024-08-20 23:31:02 +0530

    refactor(controller): remove unnecessary controller and get userId from token instead of params

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,22 +1,22 @@
+import jwt from "jsonwebtoken";
 // Import the User model
 import User from "../models/User.mjs";
 
 // Define the user controller object
 const userController = {
-  // Get all users
-  getAllUsers: async (req, res) => {
-    try {
-      const users = await User.find({ isDeleted: false });
-      return res.status(200).json({ users });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
-  },
   // Get a user
   getUser: async (req, res) => {
     try {
-      const user = await User.findById(req.params.id);
-      return res.status(200).json({ user });
+      const accessToken = req.cookies.accessToken;
+      if (!accessToken) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      // Get the user ID from the access token
+      const data = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(data.id);
+      return res
+        .status(200)
+        .json({ _id: user._id, name: user.name, email: user.email });
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
