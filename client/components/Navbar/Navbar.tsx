45fb5c5b173676810ecfd32d2cc0df8ace191873
Shishir Shekhar <sspdav02@gmail.com>
2024-08-20 23:43:05 +0530

    feat(component): add navbar component

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -5,15 +5,15 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navLinks}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/">
+          <li>About</li>
+        </Link>
+        <Link href="/">
+          <li>Contact</li>
+        </Link>
       </ul>
     </nav>
   );
