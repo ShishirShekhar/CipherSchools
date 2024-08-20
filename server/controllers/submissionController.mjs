96b223e5b96e1595a09487ca74f140f2be362c15
Shishir Shekhar <sspdav02@gmail.com>
2024-08-20 23:32:25 +0530

    fix(controller): fix required parmeters error

--- server/controllers/submissionController.mjs
+++ server/controllers/submissionController.mjs
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 // Import submission model
 import Submission from "../models/Submission.mjs";
 
@@ -15,9 +16,30 @@ const submissionController = {
   // Create a submission
   createSubmission: async (req, res) => {
     try {
-      const submission = await Submission.create(req.body);
+      const { testId, selections, endedAt } = req.body;
+      if (!testId || !selections || !endedAt) {
+        return res
+          .status(400)
+          .json({ error: "testId, selections, and endedAt are required" });
+      }
+
+      const accessToken = req.cookies.accessToken;
+      if (!accessToken) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      // Get the user ID from the access token
+      const user = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const userId = user.id;
+
+      const submission = await Submission.create({
+        testId,
+        userId,
+        selections,
+        endedAt,
+      });
       return res.status(201).json({ submission });
     } catch (error) {
+      console.log(error);
       return res.status(500).json({ error: error.message });
     }
   },
@@ -33,9 +55,13 @@ const submissionController = {
   // Update a submission
   updateSubmission: async (req, res) => {
     try {
-      const submission = await Submission.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      });
+      const submission = await Submission.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        }
+      );
       return res.status(200).json({ submission });
     } catch (error) {
       return res.status(500).json({ error: error.message });
