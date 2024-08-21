9921f3886d675467989053afa23d6d49d36cefdc
Shishir Shekhar <sspdav02@gmail.com>
2024-08-21 14:43:20 +0530

    refactor(unused code): remove unused code

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,16 +1,8 @@
-import jwt from "jsonwebtoken";
-// Import the User model
-import User from "../models/User.mjs";
-
 // Define the user controller object
 const userController = {
   // Get a user
   getUser: async (req, res) => {
     try {
-      const accessToken = req.cookies.accessToken;
-      if (!accessToken) {
-        return res.status(401).json({ data: null, error: "Unauthorized" });
-      }
       // Get the user ID from the access token
       const user = req.user;
       return res.status(200).json({
