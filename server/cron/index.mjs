34e507176728f45df4cd17d103215e5dde4304cd
Shishir Shekhar <sspdav02@gmail.com>
2024-08-21 18:21:16 +0530

    fix(cron): fix cron job v6

--- server/cron/index.mjs
+++ server/cron/index.mjs
@@ -1,9 +1,12 @@
 import cron from "../cron.mjs";
 
 // Cron job to run every hour
-const cronJob = async (req, res) => {
-  cron.start();
-  res.status(200).json({ message: "Cron job executed!" });
-};
-
-export default cronJob;
+export default async function handler(req, res) {
+  try {
+    cron.start();
+    res.status(200).json({ message: "Cron job executed!" });
+  } catch (error) {
+    console.error("Cron job failed:", error);
+    res.status(500).json({ message: "Cron job failed!" });
+  }
+}
